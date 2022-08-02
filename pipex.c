f59c7b82bc5f9eee761a230a10eb6d4bdc094a24
Piotr Biedermann <pbiederm@c2s8d27.42wolfsburg.de>
2022-08-02 18:57:38 +0200

    need err managment

--- pipex.c
+++ pipex.c
@@ -10,84 +10,122 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// needs to stay in lines and have the error handling taken care of
+
 #include <stdio.h>
 #include "./libft/libft.h"
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/errno.h>
-// Get all the paths from env
-char **ft_path_array(char** env)
+#include <string.h>
+
+char	**ft_path_array(char **env)
 {
-	int i;
-	char **path_array;
+	int		i;
+	char	**path_array;
 
 	path_array = NULL;
 	i = 0;
 	while (env[i]!=NULL)
 	{
-		if (env[i][0] == 'P' && env[i][1] == 'A' && env[i][2] == 'T' && env[i][3] == 'H' && env[i][4] == '=')
-		//if (env[i][0] == 'P' && env[i][1] == 'A' && env[i][2] == 'T' && env[i][3] == 'H')
+		if (env[i][0] == 'P' && env[i][1] == 'A' && env[i][2] ==
+		'T' && env[i][3] == 'H' && env[i][4] == '=')
 		{
 			path_array = ft_split(ft_substr(env[i], 5, ft_strlen(env[i])), ':');
-			break;
+			break ;
 		}
 		i++;
 	}
-		path_array[i] = NULL;
-		return(path_array);
+	path_array[i] = NULL;
+	return (path_array);
 }
 
-
-int main (int ac, char **av, char **env)
+// int ft_child_one(int fd0, int fd1, int pfd0, int pfd1, char **pat, char *av2)
+// {
+// 	int i;
+// 	int access_to_command;
+// 	char **command;
+// 	close(pfd0);
+// 	close(fd1);
+// 	dup2(fd0, STDIN_FILENO);
+// 	close(fd0);
+// 	dup2(pfd1, STDOUT_FILENO);
+// 	close(pfd1);
+// 	i = 0;
+// 	access_to_command = -1;
+// 	// path_array = ft_path_array(env);
+// 	command = ft_split(av2, ' ');
+// 	if (command[0] == NULL)
+// 	{
+// 		perror("No command entered");
+// 		return(667);
+// 	}
+// 	while (pat[i] != NULL)
+// 	{
+// 		access_to_command = access(ft_strjoin(pat[i], ft_strjoin("/", command[0])), F_OK);
+// 		i++;
+// 	}
+// 	if (access_to_command < 0)
+// 	{
+// 		perror("Child one wrong command: ");
+// 		// exit ;
+// 	}
+// 	return(access_to_command);
+// }
+void	ft_child_one(int fd0, int fd1, int pfd0, int pfd1)
 {
+	close(pfd0);
+	close(fd1);
+	dup2(fd0, STDIN_FILENO);
+	close(fd0);
+	dup2(pfd1, STDOUT_FILENO);
+	close(pfd1);
+}
+
+int	main (int ac, char **av, char **env)
+{
+	if (ac == 5)
+	{
+
 	char	**path_array;
 	char	**command;
-	char	*path;
 	int		i;
 	int 	fd[2];
 	int		pfd[2];
 
 	fd[0] = open(av[1], O_RDONLY);
+	if (fd[0] < 0)
+			perror("Write infile and enter: ");
 	fd[1] = open(av[4], O_CREAT | O_WRONLY, 0777);
 	if (pipe(pfd) == -1)
-	{
-		printf ("An error occured while calling for pipe\n");
-		return (1);
-	}
+		perror("Pipe failed: ");
 	int id1 = fork();
 	if(id1 == -1)
-	{
-		perror("Fork failed");
-		return (11);
-	}
-
+		perror("Fork failed: ");
 	if (id1 == 0)
 	{
-		// printf("I am protos x - parent of y protos, child of parent protos\n");
-		close(pfd[0]);
-		close(fd[1]);
-		dup2(fd[0], STDIN_FILENO);
-		close(fd[0]);
-		dup2(pfd[1], STDOUT_FILENO);
-		close(pfd[1]);
+		ft_child_one(fd[0], fd[1], pfd[0], pfd[1]);
 		i = 0;
 		path_array = ft_path_array(env);
 		command = ft_split(av[2], ' ');
-
+		if (command[0] == NULL)
+			perror("No command entered");
+		int access_to_command;
+		access_to_command = -1;
 		while (path_array[i] != NULL)
 		{
-			if(access(ft_strjoin(path_array[i], ft_strjoin("/", command[0])), F_OK) == 0)
+			access_to_command = access(ft_strjoin(path_array[i], ft_strjoin("/", command[0])), F_OK);
+			if (access_to_command == 0)
 				execve(ft_strjoin(path_array[i], ft_strjoin("/", command[0])), command, env);
 			i++;
 		}
+		perror("Child one wrong command: ");
 	}
 	else
 	{
 		int id2 = fork();
 		if (id2 == 0)
 		{
-			// printf("I am protos z second child of parent protos\n");
-			// Below goes to the second child
 			close(pfd[1]);
 			close(fd[0]);
 			dup2(pfd[0], STDIN_FILENO);
@@ -97,13 +135,6 @@ int main (int ac, char **av, char **env)
 			i = 0;
 			path_array = ft_path_array(env);
 			command = ft_split(av[3], ' ');
-			int n = 0;
-			// while( path_array[n] != NULL)
-			// {
-			// 	fprintf(2, "%s", path_array[n]);
-			// 	n++;
-			// }
-
 			while (path_array[i] != NULL)
 			{
 				if(access(ft_strjoin(path_array[i], ft_strjoin("/", command[0])), F_OK) == 0)
@@ -117,20 +148,16 @@ int main (int ac, char **av, char **env)
 			close(fd[1]);
 			close(pfd[1]);
 			close(pfd[0]);
-			// while (waitpid(id1) != -1)
-			// {
-			// 	printf("Waited for a child to finish\n");
-			// }
-			// while (waitpid(id2) != -1)
-			// {
-			// 	printf("Waited for a child to finish\n");
-			// }
 			int status;
   			waitpid(id1, &status, 0); // wait for child to finish before exiting
-			waitpid(id2, &status, 0); // wait for child to finish before exitin
-			// Parent process waits for child processes to finish
-			// printf("I am the parent protos\n");		
+			waitpid(id2, &status, 0); // wait for child to finish before exiting
 		}
+	}
+	}
+	else
+	{
+		printf("Wrong number of arrrrgs\n");
+		return(777);
 	}
 }
 
